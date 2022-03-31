d7af77d8eb8aa424ad72ee04a7e11e6a1a48bb21
callanhunter <callan57hunter@gmail.com>
2022-03-31 09:18:52 -0700

    edit password requirements in prompt

--- script.js
+++ script.js
@@ -14,6 +14,39 @@ function writePassword() {
 
 // Add event listener to generate button
 
-generateBtn.addEventListener("click", function(){prompt("How many characters do you want your password to be? min - 8, max - 128");});
+generateBtn.addEventListener("click", passwordQuestions);
+
+function passwordQuestions() {
+  var passwordLength = prompt ("How many characters do you want your password to be? min - 8, max - 128");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    return passwordLength;
+  } else {
+    return prompt("Please enter within the range of 8 - 128.");
+  }
+}
+
+function passwordQuestions() {
+  var passwordLength = prompt ("How many characters do you want your password to be? min - 8, max - 128");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    return passwordLength;
+  } else {
+    return prompt("Please enter within the range of 8 - 128.");
+  }
+}
+
+// var promptQuestion {
+//   lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz",
+//   upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+//   specialCharacters = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+// }
+
+
+
+// if (passwordLength >= 8 && passwordLength <= 128) {
+//   return passwordLength;
+// } else {
+//   return prompt("Please enter within the range of 8 - 128.");
+// }
+
 
 // generateBtn.addEventListener("click", writePassword);
